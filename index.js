f5a511bb6c9d601d410f40bf0fd60df0948c172c
ismail939 <ismailsalama526@gmail.com>
2023-11-03 17:29:06 +0200

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app=express()
+const path=require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.get('/', (req, res)=>{
+    res.render('./index.ejs')
+})
+
+
+
+app.listen(3001, ()=>{
+    console.log(`listening on port 3001`)
+})
